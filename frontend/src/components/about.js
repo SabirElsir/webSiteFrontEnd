d8076b80fb0ed8d7178faa51e6606d079cacc4b0
Awaab Elamin <AwaabElamin@gmail.com>
2024-05-23 11:12:51 -0500

    add about component

---
+++ frontend/src/components/about.js
@@ -0,0 +1,18 @@
+import '../App.css';
+import React, { Component } from 'react';
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    dfdslkfjsdlkdjf lkjdsf  lkjdskfjs l;kjdskjf ;lkjdsflkj ;lkjdsf ;lkjdsfl
+                    kljdsfkjds lkjfdlkjg lkjdfkljgfds lkl;;;kj ;lksfdjdsl
+                    jsldkjaf;lkaj khkj lkhklhj lhjsdlklk lkjlhkj llllkj lkljdslkjf
+                    ;lkjsdlkfj klfglkfglhfdklj lkjfdlkjklfsdg lllll lksjdflsd
+                    asl;kdjfksdjf kdsjkf lkjdsfj jkhsdfgj kjdsfkj kjshdkjhf lkjhdsjkfh jhskdjhfgds
+                </p>
+            </div>
+        );
+    }
+}
+export default About;
