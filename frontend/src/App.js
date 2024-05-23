efcc95900400de75d83628f968ff6037c6d0b69d
Awaab Elamin <AwaabElamin@gmail.com>
2024-05-23 11:13:58 -0500

    shown about component in main Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
+import { Link } from 'react-router-dom';
 
 function App() {
+  function aboutButtonClicked(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+  }
   return (
     <div className="App">
+      <button onClick={aboutButtonClicked}>about</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +25,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <About />
     </div>
   );
 }
